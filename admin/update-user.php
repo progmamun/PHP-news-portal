07182a11c644933e7fe26bffd353e25d27fffea6
progmamun <mamun333089@gmail.com>
2021-07-01 16:34:05 +0600

    update-user

--- admin/update-user.php
+++ admin/update-user.php
@@ -1,4 +1,4 @@
-<?php include "header.php"; ?>
+<?php include "header.php";?>
   <div id="admin-content">
       <div class="container">
           <div class="row">
@@ -6,35 +6,55 @@
                   <h1 class="admin-heading">Modify User Details</h1>
               </div>
               <div class="col-md-offset-4 col-md-4">
+                <?php
+include "config.php";
+$user_id = $_GET['id'];
+$sql = "SELECT * FROM user WHERE user_id DESC";
+$result = mysqli_query($conn, $sql1) or die("Query Failed. ");
+if (mysqli_num_rows($result) > 0) {
+    while ($row = mysqli_fetch_assoc($result)) {
+        ?>
+
                   <!-- Form Start -->
                   <form  action="" method ="POST">
                       <div class="form-group">
-                          <input type="hidden" name="user_id"  class="form-control" value="1" placeholder="" >
+                          <input type="hidden" name="user_id"  class="form-control" value="<?php echo $row['user_id']; ?>" placeholder="" >
                       </div>
                           <div class="form-group">
                           <label>First Name</label>
-                          <input type="text" name="f_name" class="form-control" value="Ram" placeholder="" required>
+                          <input type="text" name="f_name" class="form-control" value="<?php echo $row['first_name']; ?>" placeholder="" required>
                       </div>
                       <div class="form-group">
                           <label>Last Name</label>
-                          <input type="text" name="l_name" class="form-control" value="Sharma" placeholder="" required>
+                          <input type="text" name="l_name" class="form-control" value="<?php echo $row['last_name']; ?>" placeholder="" required>
                       </div>
                       <div class="form-group">
                           <label>User Name</label>
-                          <input type="text" name="username" class="form-control" value="ram" placeholder="" required>
+                          <input type="text" name="username" class="form-control" value="<?php echo $row['username']; ?>" placeholder="" required>
                       </div>
                       <div class="form-group">
                           <label>User Role</label>
                           <select class="form-control" name="role" value="<?php echo $row['role']; ?>">
-                              <option value="0">normal User</option>
-                              <option value="1">Admin</option>
+                          <?php
+if ($row['role'] == 1) {
+            echo "<option value='0'>normal User</option>
+            <option value="1" selected>Admin</option>";
+
+        } else {
+            echo "<option value='0' selected>normal User</option>
+            <option value="1">Admin</option>";
+        }
+        ?>
                           </select>
                       </div>
                       <input type="submit" name="submit" class="btn btn-primary" value="Update" required />
                   </form>
                   <!-- /Form -->
+                  <?php }
+}
+?>
               </div>
           </div>
       </div>
   </div>
-<?php include "footer.php"; ?>
+<?php include "footer.php";?>
