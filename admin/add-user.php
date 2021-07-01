260334bf966f36a7c29b87bd8a38113697d2e45d
progmamun <mamun333089@gmail.com>
2021-07-01 14:37:58 +0600

    start

--- admin/add-user.php
+++ admin/add-user.php
@@ -1,4 +1,4 @@
-<?php include "header.php"; ?>
+<?php include "header.php";?>
   <div id="admin-content">
       <div class="container">
           <div class="row">
@@ -7,7 +7,7 @@
               </div>
               <div class="col-md-offset-3 col-md-6">
                   <!-- Form Start -->
-                  <form  action="" method ="POST" autocomplete="off">
+                  <form  action="<?php $_SERVER['PHP_SELF'];?>" method ="POST" autocomplete="off">
                       <div class="form-group">
                           <label>First Name</label>
                           <input type="text" name="fname" class="form-control" placeholder="First Name" required>
@@ -39,4 +39,4 @@
            </div>
        </div>
    </div>
-<?php include "footer.php"; ?>
+<?php include "footer.php";?>
