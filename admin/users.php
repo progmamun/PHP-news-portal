17fb2094c50125e7fe733c8ecdb2471dc5313269
progmamun <mamun333089@gmail.com>
2021-07-01 16:08:24 +0600

    edit user

--- admin/users.php
+++ admin/users.php
@@ -41,8 +41,8 @@ if ($row['role'] == 1) {
             echo "Normal";
         }
         ?></td>
-                              <td class='edit'><a href='update-user.php'><i class='fa fa-edit'></i></a></td>
-                              <td class='delete'><a href='delete-user.php'><i class='fa fa-trash-o'></i></a></td>
+                              <td class='edit'><a href='update-user.php?id=<?php echo $row["user_id"]; ?>'><i class='fa fa-edit'></i></a></td>
+                              <td class='delete'><a href='delete-user.php?id=<?php echo $row["user_id"]; ?>'><i class='fa fa-trash-o'></i></a></td>
                           </tr>
                           <?php }?>
                       </tbody>
