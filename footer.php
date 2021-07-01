abc0ed6ba6724b2353fee8ce5d4deec21eb8b7b9
progmamun <mamun333089@gmail.com>
2021-07-01 13:52:54 +0600

    start

---
+++ footer.php
@@ -0,0 +1,11 @@
+<div id ="footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <span>© Copyright 2019 News | Powered by <a href="http://www.yahoobaba.net/">Yahoo Baba</a></span>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+</html>
