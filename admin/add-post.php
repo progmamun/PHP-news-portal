b1401ddddbeb7004687cdd216de75f163dab54c0
progmamun <mamun333089@gmail.com>
2021-07-03 15:43:22 +0600

    post add

--- admin/add-post.php
+++ admin/add-post.php
@@ -7,7 +7,7 @@
              </div>
               <div class="col-md-offset-3 col-md-6">
                   <!-- Form -->
-                  <form  action="" method="POST" enctype="multipart/form-data">
+                  <form  action="save-post.php" method="POST" enctype="multipart/form-data">
                       <div class="form-group">
                           <label for="post_title">Title</label>
                           <input type="text" name="post_title" class="form-control" autocomplete="off" required>
