2109004a75a16e4348ec7b43386258c838064795
progmamun <mamun333089@gmail.com>
2021-07-04 20:03:07 +0600

    root index

--- index.php
+++ index.php
@@ -7,42 +7,89 @@
                     <div class="post-container">
                     <?php
 include "config.php";
-?>
+/* Calculate offset code */
+$limit = 3;
+
+if (isset($_GET['page'])) {
+    $page = $_GET['page'];
+} else {
+    $page = 1;
+}
+$offset = ($page - 1) * $limit;
+
+$sql = "SELECT post.post_id, post.title, post.description, post.post_date, category.category_name, user.username, post.category, post.post_img FROM post
+LEFT JOIN category ON post.category = category.category_id
+LEFT JOIN user ON post.author = user.user_id
+ORDER BY post.post_id DESC LIMIT {$offset},{$limit}";
+
+$result = mysqli_query($conn, $sql) or die("Query Failed. ");
+if (mysqli_num_rows($result) > 0) {
+    while ($row = mysqli_fetch_assoc($result)) {
+        ?>
                         <div class="post-content">
                             <div class="row">
                                 <div class="col-md-4">
-                                    <a class="post-img" href="single.php"><img src="images/post-format.jpg" alt=""/></a>
+                                    <a class="post-img" href="single.php?id=<?php echo $row['post_id']; ?>"><img src="admin/upload/<?php echo $row['post_img']; ?>" alt=""/></a>
                                 </div>
                                 <div class="col-md-8">
                                     <div class="inner-content clearfix">
-                                        <h3><a href='single.php'>Lorem ipsum dolor sit amet, consectetur adipiscing elit</a></h3>
+                                        <h3><a href='single.php?id=<?php echo $row['post_id']; ?>'><?php echo $row['title']; ?></a></h3>
                                         <div class="post-information">
                                             <span>
                                                 <i class="fa fa-tags" aria-hidden="true"></i>
-                                                <a href='category.php'>PHP</a>
+                                                <a href='category.php'><?php echo $row['category_name']; ?></a>
                                             </span>
                                             <span>
                                                 <i class="fa fa-user" aria-hidden="true"></i>
-                                                <a href='author.php'>Admin</a>
+                                                <a href='author.php'><?php echo $row['username']; ?></a>
                                             </span>
                                             <span>
                                                 <i class="fa fa-calendar" aria-hidden="true"></i>
-                                                01 Nov, 2019
+                                                <?php echo $row['post_date']; ?>
                                             </span>
                                         </div>
                                         <p class="description">
-                                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua....
+                                            <?php echo $row['description']; ?>
                                         </p>
-                                        <a class='read-more pull-right' href='single.php'>read more</a>
+                                        <a class='read-more pull-right' href='single.php?id=<?php echo $row['post_id']; ?>'>read more</a>
                                     </div>
                                 </div>
                             </div>
+                            <?php
+}
+} else {
+    echo "<h2>No Record Found.</h2>";
+}
+?>
                         </div>
-                        <ul class='pagination'>
-                            <li class="active"><a href="">1</a></li>
-                            <li><a href="">2</a></li>
-                            <li><a href="">3</a></li>
-                        </ul>
+                        <?php
+//show pagination
+$sql1 = "SELECT * FROM post";
+$result1 = mysqli_query($conn, $sql1) or die("Query Failed.");
+
+if (mysqli_num_rows($result1) > 0) {
+    $total_records = mysqli_num_rows($result1);
+
+    $total_page = ceil($total_records / $limit);
+
+    echo '<ul class="pagination admin-pagination">';
+    if ($page > 1) {
+        echo '<li><a href="index.php?page=' . ($page - 1) . '">Prev</a></li>';
+    }
+    for ($i = 1; $i <= $total_page; $i++) {
+        if ($i == $page) {
+            $active = "active";
+        } else {
+            $active = "";
+        }
+        echo '<li class="' . $active . '"><a href="index.php?page=' . $i . '">' . $i . '</a></li>';
+    }
+    if ($total_page > $page) {
+        echo '<li><a href="index.php?page=' . ($page + 1) . '">Next</a></li>';
+    }
+    echo '</ul>';
+}
+?>
                     </div><!-- /post-container -->
                 </div>
                 <?php include 'sidebar.php';?>
