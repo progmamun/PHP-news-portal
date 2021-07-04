827f2f17fbeed0abbac82ddd13f6336429c9da78
progmamun <mamun333089@gmail.com>
2021-07-04 18:54:10 +0600

    config.php include

--- index.php
+++ index.php
@@ -1,190 +1,13 @@
-<?php include 'header.php'; ?>
+<?php include 'header.php';?>
     <div id="main-content">
         <div class="container">
             <div class="row">
                 <div class="col-md-8">
                     <!-- post-container -->
                     <div class="post-container">
-                        <div class="post-content">
-                            <div class="row">
-                                <div class="col-md-4">
-                                    <a class="post-img" href="single.php"><img src="images/post-format.jpg" alt=""/></a>
-                                </div>
-                                <div class="col-md-8">
-                                    <div class="inner-content clearfix">
-                                        <h3><a href='single.php'>Lorem ipsum dolor sit amet, consectetur adipiscing elit</a></h3>
-                                        <div class="post-information">
-                                            <span>
-                                                <i class="fa fa-tags" aria-hidden="true"></i>
-                                                <a href='category.php'>PHP</a>
-                                            </span>
-                                            <span>
-                                                <i class="fa fa-user" aria-hidden="true"></i>
-                                                <a href='author.php'>Admin</a>
-                                            </span>
-                                            <span>
-                                                <i class="fa fa-calendar" aria-hidden="true"></i>
-                                                01 Nov, 2019
-                                            </span>
-                                        </div>
-                                        <p class="description">
-                                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua....
-                                        </p>
-                                        <a class='read-more pull-right' href='single.php'>read more</a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="post-content">
-                            <div class="row">
-                                <div class="col-md-4">
-                                    <a class="post-img" href="single.php"><img src="images/post_1.jpg" alt=""/></a>
-                                </div>
-                                <div class="col-md-8">
-                                    <div class="inner-content clearfix">
-                                        <h3><a href='single.php'>Lorem ipsum dolor sit amet, consectetur adipiscing elit</a></h3>
-                                        <div class="post-information">
-                                            <span>
-                                                <i class="fa fa-tags" aria-hidden="true"></i>
-                                                <a href='category.php'>PHP</a>
-                                            </span>
-                                            <span>
-                                                <i class="fa fa-user" aria-hidden="true"></i>
-                                                <a href='author.php'>Admin</a>
-                                            </span>
-                                            <span>
-                                                <i class="fa fa-calendar" aria-hidden="true"></i>
-                                                01 Nov, 2019
-                                            </span>
-                                        </div>
-                                        <p class="description">
-                                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua....
-                                        </p>
-                                        <a class='read-more pull-right' href='single.php'>read more</a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="post-content">
-                            <div class="row">
-                                <div class="col-md-4">
-                                    <a class="post-img" href="single.php"><img src="images/post-format.jpg" alt=""/></a>
-                                </div>
-                                <div class="col-md-8">
-                                    <div class="inner-content clearfix">
-                                        <h3><a href='single.php'>Lorem ipsum dolor sit amet, consectetur adipiscing elit</a></h3>
-                                        <div class="post-information">
-                                            <span>
-                                                <i class="fa fa-tags" aria-hidden="true"></i>
-                                                <a href='category'>PHP</a>
-                                            </span>
-                                            <span>
-                                                <i class="fa fa-user" aria-hidden="true"></i>
-                                                <a href='author.php'>Admin</a>
-                                            </span>
-                                            <span>
-                                                <i class="fa fa-calendar" aria-hidden="true"></i>
-                                                01 Nov, 2019
-                                            </span>
-                                        </div>
-                                        <p class="description">
-                                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua....
-                                        </p>
-                                        <a class='read-more pull-right' href='single.php'>read more</a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="post-content">
-                            <div class="row">
-                                <div class="col-md-4">
-                                    <a class="post-img" href="single.php"><img src="images/post_1.jpg" alt=""/></a>
-                                </div>
-                                <div class="col-md-8">
-                                    <div class="inner-content clearfix">
-                                        <h3><a href='single.php'>Lorem ipsum dolor sit amet, consectetur adipiscing elit</a></h3>
-                                        <div class="post-information">
-                                            <span>
-                                                <i class="fa fa-tags" aria-hidden="true"></i>
-                                                <a href='category.php'>PHP</a>
-                                            </span>
-                                            <span>
-                                                <i class="fa fa-user" aria-hidden="true"></i>
-                                                <a href='author.php'>Admin</a>
-                                            </span>
-                                            <span>
-                                                <i class="fa fa-calendar" aria-hidden="true"></i>
-                                                01 Nov, 2019
-                                            </span>
-                                        </div>
-                                        <p class="description">
-                                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua....
-                                        </p>
-                                        <a class='read-more pull-right' href='single.php'>read more</a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="post-content">
-                            <div class="row">
-                                <div class="col-md-4">
-                                    <a class="post-img" href="single.php"><img src="images/post-format.jpg" alt=""/></a>
-                                </div>
-                                <div class="col-md-8">
-                                    <div class="inner-content clearfix">
-                                        <h3><a href='single.php'>Lorem ipsum dolor sit amet, consectetur adipiscing elit</a></h3>
-                                        <div class="post-information">
-                                            <span>
-                                                <i class="fa fa-tags" aria-hidden="true"></i>
-                                                <a href='category.php'>PHP</a>
-                                            </span>
-                                            <span>
-                                                <i class="fa fa-user" aria-hidden="true"></i>
-                                                <a href='author.php'>Admin</a>
-                                            </span>
-                                            <span>
-                                                <i class="fa fa-calendar" aria-hidden="true"></i>
-                                                01 Nov, 2019
-                                            </span>
-                                        </div>
-                                        <p class="description">
-                                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua....
-                                        </p>
-                                        <a class='read-more pull-right' href='single.php'>read more</a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="post-content">
-                            <div class="row">
-                                <div class="col-md-4">
-                                    <a class="post-img" href="single.php"><img src="images/post_1.jpg" alt=""/></a>
-                                </div>
-                                <div class="col-md-8">
-                                    <div class="inner-content clearfix">
-                                        <h3><a href='single.php'>Lorem ipsum dolor sit amet, consectetur adipiscing elit</a></h3>
-                                        <div class="post-information">
-                                            <span>
-                                                <i class="fa fa-tags" aria-hidden="true"></i>
-                                                <a href='category.php'>PHP</a>
-                                            </span>
-                                            <span>
-                                                <i class="fa fa-user" aria-hidden="true"></i>
-                                                <a href='author.php'>Admin</a>
-                                            </span>
-                                            <span>
-                                                <i class="fa fa-calendar" aria-hidden="true"></i>
-                                                01 Nov, 2019
-                                            </span>
-                                        </div>
-                                        <p class="description">
-                                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua....
-                                        </p>
-                                        <a class='read-more pull-right' href='single.php'>read more</a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+                    <?php
+include "config.php";
+?>
                         <div class="post-content">
                             <div class="row">
                                 <div class="col-md-4">
@@ -222,8 +45,8 @@
                         </ul>
                     </div><!-- /post-container -->
                 </div>
-                <?php include 'sidebar.php'; ?>
+                <?php include 'sidebar.php';?>
             </div>
         </div>
     </div>
-<?php include 'footer.php'; ?>
+<?php include 'footer.php';?>
