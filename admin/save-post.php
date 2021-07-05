c17c29178d56ed2e8c78997a01015baf4b33ed72
progmamun <mamun333089@gmail.com>
2021-07-05 23:55:54 +0600

    update img name

--- admin/save-post.php
+++ admin/save-post.php
@@ -8,6 +8,7 @@ if (isset($_FILES['fileToUpload'])) {
     $file_tmp = $_FILES['fileToUpload']['tmp_name'];
     $file_type = $_FILES['fileToUpload']['type'];
     $file_ext = end(explode('.', $file_name));
+
     $extensions = array("jpeg", "jpg", "png");
 
     if (in_array($file_ext, $extensions) === false) {
@@ -19,7 +20,6 @@ if (isset($_FILES['fileToUpload'])) {
     }
     $new_name = time() . "-" . basename($file_name);
     $target = "upload/" . $new_name;
-    $image_name = $new_name;
 
     if (empty($errors) == true) {
         move_uploaded_file($file_tmp, $target);
@@ -37,11 +37,11 @@ $date = date("d M, Y");
 $author = $_SESSION['user_id'];
 
 $sql = "INSERT INTO post(title, description,category,post_date,author,post_img)
-        VALUES('{$title}','{$description}',{$category},'{$date}',{$author},'{$image_name}');";
+          VALUES('{$title}','{$description}',{$category},'{$date}',{$author},'{$new_name}');";
 $sql .= "UPDATE category SET post = post + 1 WHERE category_id = {$category}";
 
 if (mysqli_multi_query($conn, $sql)) {
-    header("Location: {$hostname}/admin/post.php");
+    header("location: {$hostname}/admin/post.php");
 } else {
     echo "<div class='alert alert-danger'>Query Failed.</div>";
 }
