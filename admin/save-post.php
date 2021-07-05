630ebb3ac3c09c58cc5e9e0f345f82312a9c82b7
progmamun <mamun333089@gmail.com>
2021-07-05 23:06:58 +0600

    img name update

--- admin/save-post.php
+++ admin/save-post.php
@@ -17,9 +17,12 @@ if (isset($_FILES['fileToUpload'])) {
     if ($file_size > 2097152) {
         $errors[] = "File size must be 2mb or lower.";
     }
+    $new_name = time() . "-" . basename($file_name);
+    $target = "upload/" . $new_name;
+    $image_name = $new_name;
 
     if (empty($errors) == true) {
-        move_uploaded_file($file_tmp, "upload/" . $file_name);
+        move_uploaded_file($file_tmp, $target);
     } else {
         print_r($errors);
         die();
@@ -34,7 +37,7 @@ $date = date("d M, Y");
 $author = $_SESSION['user_id'];
 
 $sql = "INSERT INTO post(title, description,category,post_date,author,post_img)
-        VALUES('{$title}','{$description}',{$category},'{$date}',{$author},'{$file_name}');";
+        VALUES('{$title}','{$description}',{$category},'{$date}',{$author},'{$image_name}');";
 $sql .= "UPDATE category SET post = post + 1 WHERE category_id = {$category}";
 
 if (mysqli_multi_query($conn, $sql)) {
--- admin/save-update-post.php
+++ admin/save-update-post.php
@@ -2,7 +2,7 @@
 include "config.php";
 
 if (empty($_FILES['new-image']['name'])) {
-    $file_name = $_POST['old_image'];
+    $new_name = $_POST['old_image'];
 } else {
     $errors = array();
 
@@ -20,16 +20,21 @@ if (empty($_FILES['new-image']['name'])) {
     if ($file_size > 2097152) {
         $errors[] = "File size must be 2mb or lower.";
     }
+    if ($file_size > 2097152) {
+        $errors[] = "File size must be 2mb or lower.";
+    }
+    $new_name = time() . "-" . basename($file_name);
+    $target = "upload/" . $new_name;
 
     if (empty($errors) == true) {
-        move_uploaded_file($file_tmp, "upload/" . $file_name);
+        move_uploaded_file($file_tmp, "upload/" . $target);
     } else {
         print_r($errors);
         die();
     }
 }
 
-$sql = "UPDATE post SET title='{$_POST["post_title"]}',description='{$_POST["postdesc"]}',category={$_POST["category"]},post_img='{$file_name}'
+$sql = "UPDATE post SET title='{$_POST["post_title"]}',description='{$_POST["postdesc"]}',category={$_POST["category"]},post_img='{$new_name}'
 WHERE post_id={$_POST["post_id"]}";
 
 if ($_POST['old_category'] != $_POST["category"]) {
