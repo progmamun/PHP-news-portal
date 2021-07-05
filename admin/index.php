98f3b8c5a4041bc5e09ec58740f8f36cec833f3d
progmamun <mamun333089@gmail.com>
2021-07-05 17:36:35 +0600

    admin index error fixed

--- admin/index.php
+++ admin/index.php
@@ -6,6 +6,7 @@ if (isset($_SESSION["username"])) {
     header("Location: {$hostname}/admin/post.php");
 }
 ?>
+
 <!doctype html>
 <html>
    <head>
@@ -41,23 +42,31 @@ if (isset($_SESSION["username"])) {
                         <?php
 if (isset($_POST['login'])) {
     include "config.php";
-    $username = mysqli_real_escape_string($conn, $_POST['username']);
-    $password = md5($_POST['password']);
-
-    $sql = "SELECT user_id, username, role FROM user WHERE username = '{$username}' AND password = '{$password}'";
-    $result = mysqli_query($conn, $sql) or die("Query Failed.");
-
-    if (mysqli_num_rows($result) > 0) {
-        while ($row = mysqli_fetch_assoc($result)) {
-            session_start();
-            $_SESSION["username"] = $row["username"];
-            $_SESSION["user_id"] = $row["user_id"];
-            $_SESSION["user_role"] = $row["role"];
-
-            header("Localhost: {$hostname}/admin/post.php");
-        }
+    if (empty($_POST['username']) || empty($_POST['password'])) {
+        echo '<div class="alert alert-danger">All Fields must be entered.</div>';
+        die();
     } else {
-        echo '<div class="alert alert-danger">Username and Password are not match</div>';
+        $username = mysqli_real_escape_string($conn, $_POST['username']);
+        $password = md5($_POST['password']);
+
+        $sql = "SELECT user_id, username, role FROM user WHERE username = '{$username}' AND password= '{$password}'";
+
+        $result = mysqli_query($conn, $sql) or die("Query Failed.");
+
+        if (mysqli_num_rows($result) > 0) {
+
+            while ($row = mysqli_fetch_assoc($result)) {
+                session_start();
+                $_SESSION["username"] = $row['username'];
+                $_SESSION["user_id"] = $row['user_id'];
+                $_SESSION["user_role"] = $row['role'];
+
+                header("Location: {$hostname}/admin/post.php");
+            }
+
+        } else {
+            echo '<div class="alert alert-danger">Username and Password are not matched.</div>';
+        }
     }
 }
 ?>
