cb22422bcf985a0640621bbfc38ed312b5664842
progmamun <mamun333089@gmail.com>
2021-07-06 00:21:15 +0600

    fix category issue

--- category.php
+++ category.php
@@ -49,7 +49,7 @@ if (isset($_GET['cid'])) {
                                   <div class="post-information">
                                       <span>
                                           <i class="fa fa-tags" aria-hidden="true"></i>
-                                          <a href='category.php?cid=<?php echo $row['category']; ?>'><?php echo $row['category_name']; ?></a>
+                                        <a href='single.php?id=<?php echo $row['post_id']; ?>'><?php echo $row['category_name']; ?></a>
                                       </span>
                                       <span>
                                           <i class="fa fa-user" aria-hidden="true"></i>
