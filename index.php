9aa97d97d79c7eeaeb8f94368fc2c34588ae2295
progmamun <mamun333089@gmail.com>
2021-07-05 11:29:04 +0600

    author page update

--- index.php
+++ index.php
@@ -17,7 +17,7 @@ if (isset($_GET['page'])) {
 }
 $offset = ($page - 1) * $limit;
 
-$sql = "SELECT post.post_id, post.title, post.description, post.post_date, category.category_name, user.username, post.category, post.post_img FROM post
+$sql = "SELECT post.post_id, post.title, post.description, post.post_date, post.author, category.category_name, user.username, post.category, post.post_img FROM post
 LEFT JOIN category ON post.category = category.category_id
 LEFT JOIN user ON post.author = user.user_id
 ORDER BY post.post_id DESC LIMIT {$offset},{$limit}";
@@ -41,7 +41,7 @@ if (mysqli_num_rows($result) > 0) {
                                             </span>
                                             <span>
                                                 <i class="fa fa-user" aria-hidden="true"></i>
-                                                <a href='author.php'><?php echo $row['username']; ?></a>
+                                                <a href='author.php?aid=<?php echo $row['author']; ?>'><?php echo $row['username']; ?></a>
                                             </span>
                                             <span>
                                                 <i class="fa fa-calendar" aria-hidden="true"></i>
