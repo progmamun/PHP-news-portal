419425a11cbba7ab59d47e5702695956a117cf29
progmamun <mamun333089@gmail.com>
2021-07-05 18:22:15 +0600

    site update

--- header.php
+++ header.php
@@ -1,12 +1,12 @@
 <?php
+//echo "<h1>" .  . "</h1>";
 include "config.php";
-//echo "<h1>" . basename($_SERVER['PHP_SELF']) . "</h1>";
 $page = basename($_SERVER['PHP_SELF']);
 switch ($page) {
     case "single.php":
         if (isset($_GET['id'])) {
             $sql_title = "SELECT * FROM post WHERE post_id = {$_GET['id']}";
-            $result_title = mysqli_query($conn, $sql_title) or die("Title Query Failed");
+            $result_title = mysqli_query($conn, $sql_title) or die("Tile Query Failed");
             $row_title = mysqli_fetch_assoc($result_title);
             $page_title = $row_title['title'];
         } else {
@@ -16,7 +16,7 @@ switch ($page) {
     case "category.php":
         if (isset($_GET['cid'])) {
             $sql_title = "SELECT * FROM category WHERE category_id = {$_GET['cid']}";
-            $result_title = mysqli_query($conn, $sql_title) or die("Title Query Failed");
+            $result_title = mysqli_query($conn, $sql_title) or die("Tile Query Failed");
             $row_title = mysqli_fetch_assoc($result_title);
             $page_title = $row_title['category_name'] . " News";
         } else {
@@ -26,7 +26,7 @@ switch ($page) {
     case "author.php":
         if (isset($_GET['aid'])) {
             $sql_title = "SELECT * FROM user WHERE user_id = {$_GET['aid']}";
-            $result_title = mysqli_query($conn, $sql_title) or die("Title Query Failed");
+            $result_title = mysqli_query($conn, $sql_title) or die("Tile Query Failed");
             $row_title = mysqli_fetch_assoc($result_title);
             $page_title = "News By " . $row_title['first_name'] . " " . $row_title['last_name'];
         } else {
@@ -42,7 +42,10 @@ switch ($page) {
         }
         break;
     default:
-        $page_title = "News Site";
+        $sql_title = "SELECT websitename FROM settings";
+        $result_title = mysqli_query($conn, $sql_title) or die("Tile Query Failed");
+        $row_title = mysqli_fetch_assoc($result_title);
+        $page_title = $row_title['websitename'];
         break;
 }
 
@@ -72,7 +75,23 @@ switch ($page) {
         <div class="row">
             <!-- LOGO -->
             <div class=" col-md-offset-4 col-md-4">
-                <a href="index.php" id="logo"><img src="images/news.jpg"></a>
+              <?php
+include "config.php";
+
+$sql = "SELECT * FROM settings";
+
+$result = mysqli_query($conn, $sql) or die("Query Failed.");
+if (mysqli_num_rows($result) > 0) {
+    while ($row = mysqli_fetch_assoc($result)) {
+        if ($row['logo'] == "") {
+            echo '<a href="index.php"><h1>' . $row['websitename'] . '</h1></a>';
+        } else {
+            echo '<a href="index.php" id="logo"><img src="admin/images/' . $row['logo'] . '"></a>';
+        }
+
+    }
+}
+?>
             </div>
             <!-- /LOGO -->
         </div>
@@ -84,7 +103,7 @@ switch ($page) {
     <div class="container">
         <div class="row">
             <div class="col-md-12">
-            <?php
+              <?php
 include "config.php";
 
 if (isset($_GET['cid'])) {
@@ -92,13 +111,13 @@ if (isset($_GET['cid'])) {
 }
 
 $sql = "SELECT * FROM category WHERE post > 0";
-$result = mysqli_query($conn, $sql) or die("Query Failed. : Category ");
+$result = mysqli_query($conn, $sql) or die("Query Failed. : Category");
 if (mysqli_num_rows($result) > 0) {
     $active = "";
     ?>
                 <ul class='menu'>
-                <li><a href='<?php echo $hostname; ?>'>Home</a></li>
-                   <?php while ($row = mysqli_fetch_assoc($result)) {
+                  <li><a href='<?php echo $hostname; ?>'>Home</a></li>
+                  <?php while ($row = mysqli_fetch_assoc($result)) {
         if (isset($_GET['cid'])) {
             if ($row['category_id'] == $cat_id) {
                 $active = "active";
@@ -108,8 +127,8 @@ if (mysqli_num_rows($result) > 0) {
         }
         echo "<li><a class='{$active}' href='category.php?cid={$row['category_id']}'>{$row['category_name']}</a></li>";
     }?>
-                       </ul>
-                    <?php }?>
+                </ul>
+                <?php }?>
             </div>
         </div>
     </div>
